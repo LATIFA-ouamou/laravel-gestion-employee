e5519eb11bc064ee991a2aa455e07f08a900f9c4
LATIFA-ouamou <latifaouamou4444@gmail.com>
2025-10-21 22:36:04 +0100

    les blades route

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <h1>HOME</h1>
+</head>
+<body>
+    <a href="{{ route('Ajouter') }}">AJOUTE EMP</a>
+    <h1 class="text-3xl font-bold text-blue-600 text-center mt-10">
+  ✅ Tailwind CSS fonctionne dans Laravel !
+</h1>
+</body>
+</html>
